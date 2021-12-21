6ab75c59858b2a92e969623fe05f30eade17c9c4
natchavez <nathanielbchavez@gmail.com>
2021-12-21 14:22:28 +0800

    commit

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,6 +31,8 @@ const DUMMY_MEALS = [
 const AvailableMeals = (props) => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
+      key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
